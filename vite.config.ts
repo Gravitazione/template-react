23e7aa04e253a8c397a822c971cc423871a35bc8
SasipasCh <sasipas.chi@lightnet.io>
2024-11-17 06:09:40 +0700

    chore: config alias path typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite';
+/* eslint-disable import/no-nodejs-modules */
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
+  server: {
+    port: 8080,
+    host: true
+  }
 });
